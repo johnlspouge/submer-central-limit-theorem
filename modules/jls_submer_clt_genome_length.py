ad8b1335615b7ed2ec0f8762b8737e331ab28b28
johnlspouge <49167716+johnlspouge@users.noreply.github.com>
2022-03-02 14:22:25 -0500

    Added modules directory

---
+++ modules/jls_submer_clt_genome_length.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+"""
+Genomic length estimation by Stein's method and a central limit theorem for short-range dependency.
+    For input from the abstract class Syncmer jls_syncmer.
+"""
+# A. Blanca et al. (2021) 
+# The statistics of k-mers from a sequence undergoing a simple mutation process without spurious matches. 
+# DOI: 10.1101/2021.01.15.426881
+# Returns the syncmer covariances.
+
+from math import sqrt,isinf,isclose
+import numpy as np
+from scipy.optimize import bisect
+
+# Returns the point estimate, the length of the genome.
+def length_genome_mean( count_of_submers, mu_y ):
+    return count_of_submers/mu_y
+
+# Returns the left endpoint of the domains for f_minus and f_plus.
+def lower_limit_L_0( k, sigma_square_y, gamma_y ):
+    return max( k, -gamma_y/sigma_square_y)    
+
+# Returns the Wilson score intervals for approximation for L, the genome length.
+#    W_Y(L) <= -z_half_alpha
+def lambda_minus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ):
+    # Returns function f_minus for binary search.
+    def _f_minus( L ):
+        f_minus = L*mu_y
+        f_minus -= z_half_alpha*sqrt(L*sigma_square_y+gamma_y)
+        return f_minus - count_of_submers
+    L_0 = lower_limit_L_0( k, sigma_square_y, gamma_y )
+    L_star = (z_half_alpha/2.0/mu_y)**2*sigma_square_y
+    L_star -= gamma_y/sigma_square_y
+    L_tilde = max(L_0, L_star)
+    if 0.0 <= _f_minus( L_tilde ):
+        lambda_minus = [L_0, float('inf')]
+    else:
+        x = 1.0
+        while _f_minus( L_tilde+x ) < 0.0:
+            x *= 2.0
+        L_minus = bisect( _f_minus, L_tilde, L_tilde+x )
+        lambda_minus = [L_minus, float('inf')]
+    if L_0 < L_star:
+        if 0.0 <= _f_minus( L_0 ):
+            L_minus_tilde = bisect( _f_minus, L_0, L_star )
+            lambda_minus = [L_0, L_minus_tilde, L_minus, float('inf')]
+    assert isinf( lambda_minus[-1] ) 
+    return lambda_minus
+
+# Returns the Wilson score intervals for approximation for L, the genome length.
+#    W_Y(L) >= z_half_alpha
+def lambda_plus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ):
+    # Returns function f_plus for binary search.
+    def _f_plus( L ):
+        f_plus = L*mu_y
+        f_plus += z_half_alpha*sqrt(L*sigma_square_y+gamma_y)
+        return f_plus - count_of_submers
+    L_0 = lower_limit_L_0( k, sigma_square_y, gamma_y )
+    if 0.0 < _f_plus( L_0 ):
+        lambda_plus = []
+    else:
+        x = 1.0
+        while _f_plus( L_0+x ) <= 0.0:
+            x *= 2.0
+        L_plus = bisect( _f_plus, L_0, L_0+x )
+        lambda_plus = [L_0, L_plus] 
+    return lambda_plus
+
+# Returns the Wilson score intervals for approximation for L, the genome length.
+#    -z_half_alpha <= W_Y(L) <= z_half_alpha
+def lambda_zero( L_0, lambda_minus, lambda_plus ):
+    lambda_zero = lambda_minus+lambda_plus
+    lambda_zero.sort()
+    if lambda_zero[0] == L_0:
+        lambda_zero.pop(0)
+    else:
+        lambda_zero.insert(0, L_0)
+    assert isinf( lambda_zero[-1] )
+    lambda_zero.pop() # Parity in lambda_zero remains unchanged: it is still interval pairs.
+    return lambda_zero
+def _test_lambda():
+    def _W( L, count_of_submers, mu_y, sigma_square_y, gamma_y ):
+        return (count_of_submers-L*mu_y) / sqrt( L*sigma_square_y+gamma_y )
+    def _check(lambda0, value, k, count_of_submers, mu_y, sigma_square_y, gamma_y ):
+        for L_est in lambda0:
+            if not isinf( L_est ):
+                _w = _W( L_est, count_of_submers, mu_y, sigma_square_y, gamma_y )
+                assert L_est == lower_limit_L_0( k, sigma_square_y, gamma_y ) or isclose( _w, value ) 
+    sigma_square_y = 100.0
+    gamma_y = -25.0
+    z_half_alpha = 10.0
+    mu_y = 5.0
+    k = 10
+    L_0 = lower_limit_L_0( k, sigma_square_y, gamma_y )
+    assert L_0 == 10
+    counts_of_submers = [1000000, 10000, 100]
+    lambdas = [
+        [
+            [209146.50210976333, float('inf')],
+            [191253.49789023667, 209146.50210976333],
+            [10, 191253.49789023667],
+        ],[
+            [3116.460582894866, float('inf')],
+            [1283.539417105132, 3116.460582894866],
+            [10, 1283.539417105132],
+        ],[
+            [438.86068628239263, float('inf')],
+            [10, 438.86068628239263],
+            [],
+        ]
+    ]
+    for i in range(3):
+        count_of_submers = counts_of_submers[i]
+        lambda_minus0 = lambda_minus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ) 
+        lambda_plus0 = lambda_plus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ) 
+        lambda_zero0 = lambda_zero( L_0, lambda_minus0, lambda_plus0 )
+        assert np.allclose( lambda_minus0, lambdas[i][0] )
+        assert np.allclose( lambda_zero0, lambdas[i][1] )
+        assert np.allclose( lambda_plus0, lambdas[i][2] )
+        _check(lambda_minus0, -z_half_alpha, k, count_of_submers, mu_y, sigma_square_y, gamma_y)
+        _check(lambda_plus0,  +z_half_alpha, k, count_of_submers, mu_y, sigma_square_y, gamma_y)
+    k = 200
+    L_0 = lower_limit_L_0( k, sigma_square_y, gamma_y )
+    assert L_0 == 200
+    counts_of_submers = [1000000, 10000, 100]
+    lambdas = [
+        [
+            [209146.50210976333, float('inf')],
+            [191253.49789023667, 209146.50210976333],
+            [200, 191253.49789023667],
+        ],[
+            [3116.460582894866, float('inf')],
+            [1283.539417105132, 3116.460582894866],
+            [200, 1283.539417105132],
+        ],[
+            [438.86068628239263, float('inf')],
+            [200, 438.86068628239263],
+            [],
+        ]
+    ]
+    for i in range(3):
+        count_of_submers = counts_of_submers[i]
+        lambda_minus0 = lambda_minus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ) 
+        lambda_plus0 = lambda_plus( count_of_submers, z_half_alpha, k, mu_y, sigma_square_y, gamma_y ) 
+        lambda_zero0 = lambda_zero( L_0, lambda_minus0, lambda_plus0 )
+        assert np.allclose( lambda_minus0, lambdas[i][0] )
+        assert np.allclose( lambda_zero0, lambdas[i][1] )
+        assert np.allclose( lambda_plus0, lambdas[i][2] )
+        _check(lambda_minus0, -z_half_alpha, k, count_of_submers, mu_y, sigma_square_y, gamma_y)
+        _check(lambda_plus0,  +z_half_alpha, k, count_of_submers, mu_y, sigma_square_y, gamma_y)
+def _test_lambda_zero():
+    assert lambda_zero( 2, [3,4,7,float('inf')], [5,6,9,10] ) == [2, 3, 4, 5, 6, 7, 9, 10]
+    assert lambda_zero( 2, [2,3,7,float('inf')], [5,6,9,10] ) == [3, 5, 6, 7, 9, 10]
+
+def main(): 
+   _test_lambda()
+   _test_lambda_zero()
+    
+if __name__ == "__main__":
+    main()
---
+++ modules/jls_syncmer_closed.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""
+Syncmer_Closed class
+"""
+
+from math import isclose
+import numpy as np
+from jls_syncmer import Syncmer
+
+# R. Edgar (2021) 
+# Syncmers are more sensitive than minimizers for selecting conserved k‑mers in biological sequences. 
+# PeerJ 9: e10805.
+class Syncmer_Closed(Syncmer): 
+    def __init__(self, k, s): # k-mer with s-minimizer at 0-offset index 0 or u
+        super().__init__(k, s)
+    # Returns syncmer indicator for index i.
+    def indicator(self, codes):
+        u = self.u
+        assert len(codes) == u+1
+        minimizer_value = min(codes[0],codes[u])
+        for j in range( 1, u ):
+            if codes[j] < minimizer_value:
+                return 0
+        return 1
+    # Returns estimates of expectation_product_indicators as a list[0...k).
+    #     E[ Y[0]*Y[i] ] = E[ Y[0] ]*E[ Y[i] ] for i > u=k-s.
+    def expectation_product_indicator(self,i):
+        u = self.u
+        p = 2.0/(u+1)
+        if i == 0:
+            return p
+        elif i < u:
+            return 4.0/(i+u+1)/(u+1)
+        elif i == u:
+            return 1.0/(2*u+1) + 4.0/(i+u+1)/(u+1)
+        return p*p
+    # Returns the syncmer first passage probabilities, aka, nearest-neighbor distance distribution.
+    #     P[ Y[i]=1 and Y[j]=0 for 0<j<i | Y[0]=1 ] 
+    def first_passage_probability(self,i): # [0, 1/3, 4/21, 5/42, 5/14, 0] for k=6, u=4 
+        u = self.u
+        p = self.probability()
+        if i == 0:
+            return 0.0  
+        elif i < u:
+            return 4.0*u/(i+u+1)/(i+u)/(i+u-1)/p
+        elif i == u:
+            return (1.0/(2*u+1) + 4.0*u/(i+u+1)/(i+u)/(i+u-1))/p
+        return 0.0
+    # Returns the probability of an alpha-test window containing a closed syncmer.
+    def to_test_probability_analytic(u,alpha):
+        if alpha <= u:
+            return (2*alpha)/(u+alpha)
+        return 1.0
+
+def _test_Syncmer_Closed():
+    syncmer_closed = Syncmer_Closed(6,2) # Each syncmer has k-s+1 = u+1 codes.
+    assert syncmer_closed.name() == 'Syncmer_Closed' 
+    (syncmer_closed.get_k(),syncmer_closed.s,syncmer_closed.u) == (6,2,4)  
+    (k,u) = (6,4) 
+    # Tests indicator() and indicators() by hand calculation.
+    assert syncmer_closed.indicator((0,1,2,3,4.0)) == 1
+    assert syncmer_closed.indicator((1,2,3,4,0)) == 1
+    assert syncmer_closed.indicator((1,0,2,4,3)) == 0
+    assert syncmer_closed.indicators((1,2,3,4,0,5)) == [1,0]
+    assert syncmer_closed.indicators((-1,1,2,3,4,0,5)) == [1,1,0]
+    # Tests probability() by hand calculation.
+    p0 = 0.4 # syncmer density0
+    p = syncmer_closed.probability() # syncmer density
+    assert isclose( p, p0 )
+    # Tests expectation_product_indicators by hand calculation.
+    epis0 = [2/5, 2/15, 4/35, 1/10, 1/5, 4/25] # expectation_product_indicators0
+    epis = [ syncmer_closed.expectation_product_indicator(i) for i in range(k) ] # expectation_product_indicators
+    assert np.allclose( epis, epis0, atol=1.0e-08 )
+    # Tests covariances without mutation, cov(Y[0],Y[i]).
+    covs0 = [epi-p*p for epi in epis0] # covariances0
+    covs = [syncmer_closed.covariance(i) for i in range(len(covs0))] # covariances
+    assert np.allclose( covs, covs0, atol=1.0e-08 )
+    # Tests covariances with mutation, cov(Y[0]*Theta_bar[0],Y[i]*Theta_bar[i]).
+    theta = 0.9
+    theta_bar = 1.0-theta
+    mutation_alone_covariances = [ theta_bar**(i+k)-theta_bar**(2*k) for i in range(k) ] # mutation covariances cov(Theta_bar[0],Theta_bar[i]) without syncmers
+    mutation_covariances0 = [ a*b for a,b in zip(epis, mutation_alone_covariances) ]
+    mutation_covariances = [ syncmer_closed.mutation_covariance(i,theta) for i in range(k)]
+    assert np.allclose( mutation_covariances, mutation_covariances0, rtol=1.0e-05 )
+    # Tests first_passage_probabilities, f[i] by hand calculation.
+    fpps0 = [0, 1/3, 4/21, 5/42, 5/14, 0] # first_passage_probabilities0
+    fpps = [ syncmer_closed.first_passage_probability(i) for i in range(k) ] # first_passage_probabilities
+    assert isclose(sum(fpps0), 1.0)
+    assert isclose(sum(fpps), 1.0)
+    assert np.allclose(fpps, fpps0)
+    assert isclose(syncmer_closed.first_passage_moment(0), 1.0) # sum(fpps0)
+    assert isclose(syncmer_closed.probability() * syncmer_closed.first_passage_moment(1), 1.0) # sum(i*fpps0[i])
+    assert isclose(syncmer_closed.first_passage_moment(2), 7.88095238095238) # sum((i**2)*fpps0[i])
+    # Tests to_test_probability_analytic with Shaw's formula.
+    length = len(fpps)
+    tps0 = [ Syncmer_Closed.to_test_probability_analytic(u,alpha) for alpha in range(length) ] # test_probabilities0
+    tps = Syncmer.to_test_probabilities(p,fpps) # test_probabilities
+    assert np.allclose(tps,tps0)
+    
+def _test_Syncmer_Closed_Simulate():
+    r = 100000 #(Monte Carlo realizations)
+    np.random.seed(31415)
+
+    syncmer_closed = Syncmer_Closed(6,2)
+    # Tests expectation_product_indicators by simulation.
+    epis_mc = syncmer_closed.expectation_product_indicators_monte_carlo(r) 
+    epis0 = [ syncmer_closed.expectation_product_indicator(i) for i in range(len(epis_mc)) ]
+    assert np.allclose( epis_mc, epis0, atol=1.0e-03 )
+    # Tests first_passage_probabilities by simulation.
+    fpps_mc = syncmer_closed.first_passage_probabilities_monte_carlo(r)
+    fpps0 = [ syncmer_closed.first_passage_probability(i) for i in range(len(fpps_mc)) ]
+    assert isclose(sum(fpps_mc), 1.0)
+    assert isclose(sum(fpps0), 1.0)
+    assert np.allclose(fpps_mc, fpps0, atol=1.0e-03 )
+
+def main(): 
+    _test_Syncmer_Closed()
+    _test_Syncmer_Closed_Simulate()
+    
+if __name__ == "__main__":
+    main()
